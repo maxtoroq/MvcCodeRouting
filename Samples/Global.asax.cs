0a43d56ef3d3d6d5470276a71f9043f3c8972787
maxtoroq <none@none.com>
2011-04-18 00:33:52 +0000

    Added samples application.

---
+++ Samples/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcCodeRouting;
+
+namespace Samples {
+
+   public class MvcApplication : System.Web.HttpApplication {
+
+      void Application_Start() {
+
+         RegisterRoutes(RouteTable.Routes);
+         RegisterViewEngines(ViewEngines.Engines);
+      }
+
+      void RegisterRoutes(RouteCollection routes) {
+
+         routes.RouteExistingFiles = true;
+
+         routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+         routes.MapCodeRoutes(
+            baseNamespace: typeof(MvcApplication).Namespace
+         );
+      }
+
+      void RegisterViewEngines(ViewEngineCollection viewEngines) {
+         viewEngines.EnableCodeRouting();
+      }
+   }
+}
