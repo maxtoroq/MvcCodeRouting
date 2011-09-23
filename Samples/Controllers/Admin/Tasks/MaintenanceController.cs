598e5a5b715d8df2f2b0cd3c2a95175d3db3b345
maxtoroq <none@none.com>
2011-09-23 02:56:56 +0000

    - Fixed #535. - Hierarchical URLs (aka. RESTful) initial implementation.

---
+++ Samples/Controllers/Admin/Tasks/MaintenanceController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Samples.Controllers.Admin.Tasks {
+   
+   public class MaintenanceController : Controller {
+
+      public ActionResult Index() {
+         throw new NotImplementedException();
+      }
+   }
+}
---
+++ Samples/Controllers/Book/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCodeRouting;
+
+namespace Samples.Controllers.Book {
+
+   public class BookController : Controller {
+
+      [FromRoute]
+      public int BookId { get; set; }
+
+      public ActionResult Index() {
+         throw new NotImplementedException();
+      }
+
+      [HttpDelete]
+      public ActionResult Index(string foo) {
+         throw new NotImplementedException();
+      }
+
+      public ActionResult Chapter([FromRoute]int chapterId, [FromRoute]int page = 1) {
+         throw new NotImplementedException();
+      }
+   }
+}
