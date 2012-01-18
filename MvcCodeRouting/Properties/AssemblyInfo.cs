bac201d1df033754e7f9e8d00e21929aba547ec8
maxtoroq <none@none.com>
2012-01-18 18:34:12 +0000

    AssemblyInfo edit.

--- MvcCodeRouting/Properties/AssemblyInfo.cs
+++ MvcCodeRouting/Properties/AssemblyInfo.cs
@@ -1,28 +1,10 @@
-﻿// Copyright 2011 Max Toro Q.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-using System;
+﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: AssemblyVersion("0.9.4")]
-[assembly: AssemblyProduct("MvcCodeRouting")]
 [assembly: AssemblyTitle("MvcCodeRouting.dll")]
 [assembly: AssemblyDescription("MvcCodeRouting.dll")]
-[assembly: AssemblyCopyright("© 2011-2012 Max Toro Q.")]
-[assembly: AssemblyCompany("http://mvccoderouting.codeplex.com/")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliantAttribute(true)]
 
