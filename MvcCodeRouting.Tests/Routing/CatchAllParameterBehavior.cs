ce6991b42aedef4f2e1ac5f4bd6e1bff6ab2181d
maxtoroq <none@none.com>
2012-03-27 04:56:27 +0000

    Some tests.

---
+++ MvcCodeRouting.Tests/Routing/CatchAllParameterBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcCodeRouting.Tests.Routing {
+   
+   [TestClass]
+   public class CatchAllParameterBehavior {
+
+      [TestMethod]
+      public void SupportsCatchAllParameter() {
+
+         var routes = new RouteCollection();
+         routes.MapCodeRoutes(typeof(CatchAllParameter1Controller), new CodeRoutingSettings { RootOnly = true });
+
+         Assert.IsTrue(((Route)routes[0]).Url.EndsWith("{*a}"));
+      }
+
+      [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+      public void IsLastParameter() {
+
+         var routes = new RouteCollection();
+         routes.MapCodeRoutes(typeof(CatchAllParameter2Controller), new CodeRoutingSettings { RootOnly = true });
+      }
+   }
+
+   public class CatchAllParameter1Controller : Controller {
+
+      public void Foo([FromRoute(CatchAll = true)]string a) { }
+   }
+
+   public class CatchAllParameter2Controller : Controller {
+
+      public void Foo([FromRoute(CatchAll = true)]string a, [FromRoute]string b) { }
+   }
+}
