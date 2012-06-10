a22d9517df50237fd76ec9b606396d651d277236
maxtoroq <none@none.com>
2012-06-10 23:24:35 +0000

    Samples changes.

--- samples/Controllers/HomeController.cs
+++ samples/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using MvcCodeRouting;
@@ -11,6 +12,13 @@ namespace Samples.Controllers {
 
       public ActionResult Index() {
          return View();
+      }
+
+      public ActionResult MvcVersion() { 
+         
+         var assembly = typeof(Controller).Assembly;
+
+         return Content(String.Format("ASP.NET MVC version: {0} ({1})", assembly.GetName().Version, ((AssemblyFileVersionAttribute)assembly.GetCustomAttributes(typeof(AssemblyFileVersionAttribute), false)[0]).Version));
       }
    }
 }
