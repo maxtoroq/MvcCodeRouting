6a00073ef7a34d91156ca6d983e01a71103147a0
Max Toro <maxtoroq@gmail.com>
2012-09-25 22:19:51 -0300

    samples updated to mvc4

--- samples/Global.asax.cs
+++ samples/Global.asax.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -10,26 +9,12 @@ using MvcCodeRouting;
 namespace Samples {
 
    public class MvcApplication : System.Web.HttpApplication {
+      
+      protected void Application_Start() {
 
-      void Application_Start() {
-
-         RegisterRoutes(RouteTable.Routes);
-         RegisterViewEngines(ViewEngines.Engines);
-      }
-
-      void RegisterRoutes(RouteCollection routes) {
-
-         routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-         CodeRoutingSettings.Defaults.Reset();
-         CodeRoutingSettings.Defaults.RouteFormatter = args =>
-            Regex.Replace(args.OriginalSegment, @"([a-z])([A-Z])", "$1-$2").ToLowerInvariant();
-
-         routes.MapCodeRoutes(typeof(Controllers.HomeController));
-      }
-
-      void RegisterViewEngines(ViewEngineCollection viewEngines) {
-         viewEngines.EnableCodeRouting();
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+         FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+         ViewEngineConfig.RegisterViewEngines(ViewEngines.Engines);
       }
    }
 }
