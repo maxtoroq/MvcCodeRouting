90b0866de9f02f411b9fc6a9b3488711d7aa46cf
Max Toro <maxtoroq@gmail.com>
2012-09-26 22:50:53 -0300

    crud controllers to admin namespace

--- samples/Controllers/Api/RoleController.cs
+++ samples/Controllers/Api/RoleController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Samples.Controllers.Api {
+namespace Samples.Controllers.Admin {
    
    public class RoleController : CrudController<Role, int> { }
 
