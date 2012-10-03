0d78587fc7ac22a661e02eb1f9319ef6973e5556
Max Toro <maxtoroq@gmail.com>
2012-10-02 23:50:22 -0300

    file moves based on namespace refactor

--- MvcCodeRouting/CodeRoute.cs
+++ MvcCodeRouting/CodeRoute.cs
@@ -14,12 +14,10 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Web;
-using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MvcCodeRouting {
