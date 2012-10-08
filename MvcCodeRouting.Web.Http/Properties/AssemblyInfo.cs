141871191b5f8fb91a366f648f6d0f104acbadd2
Max Toro <maxtoroq@gmail.com>
2012-10-07 23:22:29 -0300

    webapi project

---
+++ MvcCodeRouting.Web.Http/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: AssemblyTitle("MvcCodeRouting.Web.Http.dll")]
+[assembly: AssemblyDescription("MvcCodeRouting.Web.Http.dll")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliantAttribute(true)]
+
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "MvcCodeRouting", MessageId = "Mvc", Justification = "Term is recognized.")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Mvc", Justification = "Term is recognized.")]
