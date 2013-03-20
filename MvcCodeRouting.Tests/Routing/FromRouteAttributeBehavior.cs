2e2d44fb58f26e6553942383885f1ccf40dcf979
Max Toro <maxtoroq@gmail.com>
2013-03-20 02:42:58 -0300

    tests

---
+++ MvcCodeRouting.Tests/Routing/FromRouteAttributeBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MvcCodeRouting.Tests.Routing {
+   
+   [TestClass]
+   public class FromRouteAttributeBehavior {
+
+      readonly RouteCollection routes;
+      readonly UrlHelper Url;
+
+      public FromRouteAttributeBehavior() {
+
+         routes = TestUtil.GetRouteCollection();
+         Url = TestUtil.CreateUrlHelper(routes);
+      }
+
+      [TestMethod]
+      public void UseCustomName() {
+
+         var controller = typeof(FromRouteAttr.FromRouteAttributeController);
+
+         routes.Clear();
+         routes.MapCodeRoutes(controller);
+
+         Assert.IsNotNull(routes.At(0).Url.Contains("{b}"));
+      }
+
+      [TestMethod]
+      public void BindCustomName() {
+
+         var controller = typeof(FromRouteAttr.FromRouteAttributeController);
+
+         routes.Clear();
+         routes.MapCodeRoutes(controller);
+
+         var httpContextMock = new Mock<HttpContextBase>();
+         httpContextMock.Setup(c => c.Request.AppRelativeCurrentExecutionFilePath).Returns("~/Foo/hello");
+
+         var httpResponseMock = new Mock<HttpResponseBase>();
+         httpContextMock.Setup(c => c.Response).Returns(httpResponseMock.Object);
+         
+         var routeData = routes.GetRouteData(httpContextMock.Object);
+         
+         var controllerInstance = new FromRouteAttr.FromRouteAttributeController { 
+            ValidateRequest = false
+         };
+         
+         var controllerContext = new ControllerContext(httpContextMock.Object, routeData, controllerInstance);
+
+         controllerInstance.ValueProvider = new ValueProviderCollection(new IValueProvider[] { new RouteDataValueProvider(controllerContext) });
+
+         controllerInstance.ActionInvoker.InvokeAction(controllerContext, routeData.GetRequiredString("action"));
+
+         httpResponseMock.Verify(c => c.Write(It.Is<string>(s => s == "hello")), Times.AtLeastOnce());
+      }
+   }
+}
+
+namespace MvcCodeRouting.Tests.Routing.FromRouteAttr {
+
+   public class FromRouteAttributeController : Controller {
+
+      public string Foo([FromRoute("b")]string a) {
+         return a;
+      }
+   }
+}
---
+++ MvcCodeRouting.Web.Http.Tests/Routing/FromRouteAttributeBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dependencies;
+using System.Web.Http.Hosting;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvcCodeRouting.Web.Http.Tests.Routing {
+   
+   [TestClass]
+   public class FromRouteAttributeBehavior {
+
+      [TestMethod]
+      public void UseCustomName() {
+
+         var controller = typeof(FromRouteAttr.FromRouteAttributeController);
+
+         var config = new HttpConfiguration();
+         var routes = config.Routes;
+
+         var settings = new CodeRoutingSettings();
+         settings.HttpConfiguration(config);
+
+         routes.MapCodeRoutes(controller, settings);
+
+         Assert.IsNotNull(routes.First().RouteTemplate.Contains("{b}"));
+      }
+
+      [TestMethod]
+      public void BindCustomName() {
+
+         var controller = typeof(FromRouteAttr.FromRouteAttributeController);
+
+         var config = new HttpConfiguration();
+         var routes = config.Routes;
+         
+         var settings = new CodeRoutingSettings();
+         settings.HttpConfiguration(config);
+
+         routes.MapCodeRoutes(controller, settings);
+         
+         var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/hello");
+         var routeData = routes.GetRouteData(request);
+
+         var controllerInstance = new FromRouteAttr.FromRouteAttributeController();
+
+         var controllerContext = new HttpControllerContext(config, routeData, request) {
+            ControllerDescriptor = new HttpControllerDescriptor(config, (string)routeData.Values["controller"], controller),
+            Controller = controllerInstance
+         };
+
+         var result = controllerInstance.ExecuteAsync(controllerContext, CancellationToken.None).Result;
+         string value;
+
+         Assert.IsTrue(result.TryGetContentValue(out value) && value == "hello");
+      }
+   }
+}
+
+namespace MvcCodeRouting.Web.Http.Tests.Routing.FromRouteAttr {
+
+   public class FromRouteAttributeController : ApiController {
+
+      public string Get([FromRoute("b")]string a) {
+         return a;
+      }
+   }
+}
