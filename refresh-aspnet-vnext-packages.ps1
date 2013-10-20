7eaf3656dfac1b9bffe57d1750d61465f75d1ff0
Max Toro <maxtoroq@gmail.com>
2013-10-19 23:25:52 -0300

    fixed aspnet-vnext references

--- refresh-aspnet-vnext-packages.ps1
+++ refresh-aspnet-vnext-packages.ps1
@@ -2,6 +2,7 @@
 $output = "packages\aspnet-vnext"
 Remove-Item $output\*.* -Recurse -ErrorAction SilentlyContinue
 
+.\.nuget\NuGet.exe install Microsoft.Web.Infrastructure -Version 1.0.0.0 -OutputDirectory $output -ExcludeVersion
 .\.nuget\NuGet.exe install Microsoft.AspNet.Mvc -Prerelease -OutputDirectory $output -ExcludeVersion -Source $nightly
 .\.nuget\NuGet.exe install Microsoft.Net.Http -Prerelease -OutputDirectory $output -ExcludeVersion
 .\.nuget\NuGet.exe install Microsoft.AspNet.WebApi.WebHost -Prerelease -OutputDirectory $output -ExcludeVersion -Source $nightly
