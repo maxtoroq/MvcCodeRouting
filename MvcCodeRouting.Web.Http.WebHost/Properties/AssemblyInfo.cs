67fe3a76b3162ace87f42a4f58961b8189544b87
Max Toro <maxtoroq@gmail.com>
2013-12-04 01:19:16 -0300

    moved Web.Http.WebHost to separate project

---
+++ MvcCodeRouting.Web.Http.WebHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Web;
+using MvcCodeRouting.Web.Http.WebHost;
+
+[assembly: AssemblyTitle("MvcCodeRouting.Web.Http.WebHost.dll")]
+[assembly: AssemblyDescription("MvcCodeRouting.Web.Http.WebHost.dll")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliantAttribute(true)]
+[assembly: PreApplicationStartMethod(typeof(PreApplicationStartCode), "Start")]
