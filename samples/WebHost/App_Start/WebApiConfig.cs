451d980a1813a3710eb1e769113750fc0c3ee78a
Max Toro <maxtoroq@gmail.com>
2013-12-13 22:01:39 -0300

    solution fixes

---
+++ samples/WebHost/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Samples {
+
+   public class WebApiConfig {
+
+      public static void Register(HttpConfiguration config) {
+
+         // XML displays better on the browser
+
+         var jsonMediaTypeFormatters = config.Formatters
+             .Where(f => f.SupportedMediaTypes.Any(m => m.MediaType.Equals("application/json", StringComparison.InvariantCultureIgnoreCase)))
+             .ToList();
+
+         foreach (var formatter in jsonMediaTypeFormatters) {
+            config.Formatters.Remove(formatter);
+         }
+      }
+   }
+}
--- samples/WebHost/Global.asax.cs
+++ samples/WebHost/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using MvcCodeRouting;
+using System.Web.Http;
 
 namespace Samples {
 
@@ -15,6 +16,7 @@ namespace Samples {
          RouteConfig.RegisterRoutes(RouteTable.Routes);
          FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
          ViewEngineConfig.RegisterViewEngines(ViewEngines.Engines);
+         WebApiConfig.Register(GlobalConfiguration.Configuration);
       }
    }
 }
