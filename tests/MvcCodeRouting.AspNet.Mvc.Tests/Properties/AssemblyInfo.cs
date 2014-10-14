4e87fc7bc570c5722b7c35019db414e00521975c
Max Toro <maxtoroq@gmail.com>
2014-10-14 01:41:58 -0300

    dev

---
+++ tests/MvcCodeRouting.AspNet.Mvc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MvcCodeRouting.AspNet.Mvc.Tests.dll")]
+[assembly: AssemblyDescription("MvcCodeRouting.AspNet.Mvc.Tests.dll")]
+[assembly: ComVisible(false)]
---
+++ tests/MvcCodeRouting.AspNet.Mvc.Tests/Routing/RoutingFacts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Routing.Template;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MvcCodeRouting.AspNet.Mvc.Tests.Routing {
+   
+   [TestClass]
+   public class RoutingFacts {
+
+      RouteBuilder builder;
+
+      public RoutingFacts() {
+
+         var serviceProviderMock = new Mock<IServiceProvider>();
+
+         this.builder = new RouteBuilder { 
+            DefaultHandler = new MvcRouteHandler(),
+            ServiceProvider = serviceProviderMock.Object
+         };
+      }
+
+      [TestMethod]
+      public void NameIsOptional() {
+
+         builder.MapRoute(null, "");
+      }
+
+      [TestMethod]
+      public async Task ParametersAreRequiredByDefault() {
+
+         builder.MapRoute(null, "{a}");
+
+         var routes = builder.Build();
+
+         RouteContext context = CreateRouteContext("/");
+
+         await routes.RouteAsync(context);
+
+         Assert.IsFalse(context.IsHandled);
+      }
+
+      static RouteContext CreateRouteContext(string requestPath) {
+
+         var request = new Mock<HttpRequest>(MockBehavior.Strict);
+         request.SetupGet(r => r.Path).Returns(new PathString(requestPath));
+
+         var context = new Mock<HttpContext>(MockBehavior.Strict);
+         context.SetupGet(c => c.Request).Returns(request.Object);
+
+         return new RouteContext(context.Object);
+      }
+   }
+}
